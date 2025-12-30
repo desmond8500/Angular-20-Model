db0f7adac3bc5c186db27a44fab6d628e69a641f
kratos <desmond8500@gmail.com>
2025-12-30 11:42:33 +0000

    Revert "invoice update"
    This reverts commit 2c65ff569db17ff4cc917521e06421ed172e7c7a.

--- src/app/pages/erp/invoice-card/invoice-card.ts
+++ src/app/pages/erp/invoice-card/invoice-card.ts
@@ -1,19 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
-import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-invoice-card',
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule],
   templateUrl: './invoice-card.html',
-  styleUrl: './invoice-card.scss',
+  styleUrl: './invoice-card.scss'
 })
 export class InvoiceCard {
-  facture = input<any>();
-  // totalCost: number = this.facture().spents.reduce(
-  //   (accumulator:any, product:any) => accumulator + product.montant,
-  //   0
-  // );
+  facture = input<any>()
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
