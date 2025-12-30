60ebf20b419d639445d916d5e36eab9aa4280e77
kratos <desmond8500@gmail.com>
2025-12-30 11:42:33 +0000

    invoice update

--- src/app/pages/erp/invoice-card/invoice-card.ts
+++ src/app/pages/erp/invoice-card/invoice-card.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-invoice-card',
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule],
   templateUrl: './invoice-card.html',
-  styleUrl: './invoice-card.scss'
+  styleUrl: './invoice-card.scss',
 })
 export class InvoiceCard {
-  facture = input<any>()
+  facture = input<any>();
+  // totalCost: number = this.facture().spents.reduce(
+  //   (accumulator:any, product:any) => accumulator + product.montant,
+  //   0
+  // );
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
